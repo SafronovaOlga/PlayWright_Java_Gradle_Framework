3ca2e07a7223b4ef48c56783fa00e5906b07be17
new_name <new_email>
2025-09-19 15:46:23 +0200

    Added pages.packege, components, models

---
+++ src/test/java/com/components/SideNavigationMenu.java
@@ -0,0 +1,1 @@
+package com.components;import com.microsoft.playwright.Page;public final class SideNavigationMenu extends BaseComponent {        public SideNavigationMenu (final Page page) {        super(page);    }        public void clickOnLogout() {        page.click("#logout_sidebar_link");    }}
